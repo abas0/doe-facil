b006dc17d888e1b7235493fed0274bcc279de704
Ana Beatriz Araújo de Sousa <ana.araujo.de.sousa@avanade.com>
2021-11-25 21:55:41 -0300

    imagem de fundo no obrigado.dart

--- lib/screens/obrigado-cadastro/widget/obrigado_body.dart
+++ lib/screens/obrigado-cadastro/widget/obrigado_body.dart
@@ -1,43 +1,102 @@
 import 'package:doe_facil/constants/constants.dart';
 import 'package:flutter/material.dart';
 
-double _height = 0.0;
-double _width = 0.0;
-
 class ObrigadoBody extends StatelessWidget {
   const ObrigadoBody({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    _width = MediaQuery.of(context).size.width;
-    _height = MediaQuery.of(context).size.height;
-    return Container(
-      child: new Column(
-        children: <Widget>[
-          new Container(
-            height: _height * .35,
-            color: Colors.yellow,
-          ),
-          new Container(
-            height: _height * .56,  
-            color: Colors.yellow,
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: kBackgroundColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(40.0),
-                        topRight: const Radius.circular(40.0),
-                        )
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container(
+          height: size.height * 0.32,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                  left: 100 + kDefaultPadding,
+                  right: kDefaultPadding,
+                  //top: 10,
+                ),
+                height: size.height * 0.3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      image: AssetImage("assets/images/top.png")),
+                ),
+               
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 100 + kDefaultPadding,
+                  right: kDefaultPadding,
+                  bottom: kDefaultPadding,
+                  top: 50 + kDefaultPadding,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'Doe Fácil',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Flexible(
+                      child: Text(
+                        'Conectando quem pode doar a quem precisa',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                Container(
+                  ],
+                ),
+              ),
+              Container(
+                height: 160,
+                padding: EdgeInsets.only(
+                  left: kDefaultPadding,
+                  right: kDefaultPadding,
+                  bottom: kDefaultPadding,
+                  top: kDefaultPadding + 35,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("assets/images/appicon.png")
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: kBackgroundColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryLight.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+            child: Container(
                 padding: EdgeInsets.only(
                   left: 30,
                   right: kDefaultPadding,
-                  bottom: kDefaultPadding,
-                  top: 42,
+                  top: 15,
                 ),
                 child: Column(
                   children: <Widget>[
@@ -56,66 +115,13 @@ class ObrigadoBody extends StatelessWidget {
                   ]
                 )
                 ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'Obrigada por se cadastrar',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 17.0,
-                              color: Colors.grey[900],
-                          ),
-                        ),
-                      ]
-                    ),
-                  ]
-                  ),
                 ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(
-                              top: 40
-                            ),
-                            child: Column(
-                              children: <Widget>[
-                          Text(
-                            'Fique de olho, em breve você receberá uma doação!',
-                            
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.grey[900],
-                          ),
-                        ),
-                      ]
-                            )
-                          ),
-                        ]
-                    ),
-                  ]
-                  ),
-                )
-              ],
-            ),
-            
-          )
-        ],
-      )
+              ),
+              
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
